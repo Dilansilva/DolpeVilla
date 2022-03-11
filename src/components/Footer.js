ddc2026cd97f7658306f694c5c7a31c252d4a80c
Dilansilva <sdilansilva@gmail.com>
2022-03-11 23:00:19 +0530

    Main footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,79 @@
+import React from "react";
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBCol,
+    MDBRow,
+    MDBIcon
+  } from 'mdb-react-ui-kit';
+
 function Footer() {
-    return ( 
-    <div>
-        <p>Footer component</p>
-    </div>
-    );
+    return (
+        <MDBFooter className='bg-light text-center text-white'>
+          <div className='container p-4 pb-0'>
+            <section className='mb-4'>
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#3b5998' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='facebook-f' />
+              </a>
+    
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#55acee' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='twitter' />
+              </a>
+    
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#dd4b39' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='google' />
+              </a>
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#ac2bac' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='instagram' />
+              </a>
+    
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#0082ca' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='linkedin-in' />
+              </a>
+    
+              <a
+                className='btn btn-primary btn-floating m-1'
+                style={{ backgroundColor: '#333333' }}
+                href='#!'
+                role='button'
+              >
+                <MDBIcon fab icon='github' />
+              </a>
+            </section>
+          </div>
+    
+          <div className='text-center p-1' style={{ backgroundColor: '#1F211F' }}>
+          All Copyrights Reserved by Dolape Villa Ella - © 2022
+            <a className='text-white' href='https://mdbootstrap.com/'>
+              MDBootstrap.com
+            </a>
+          </div>
+        </MDBFooter>
+      );
 }
 export default Footer;
