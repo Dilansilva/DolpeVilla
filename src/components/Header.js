d6c392f70371a91e1455bbb9e55cc97c1cca9901
Dilansilva <sdilansilva@gmail.com>
2022-03-23 18:55:49 +0530

    code refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,9 @@ import {Container,Navbar,Nav,NavDropdown} from 'react-bootstrap';
 function Header(){
  return(
   <div>
-     <Navbar className='upperDiv hiddenUpperDiv' expand="lg">
-         {
-    //     <div>
-    //     {/*upper division*/}
-    //     <div className="upperDiv hiddenUpperDiv">
-    //     <p className='blackFont'>&nbsp</p>
-    //     </div>
-    //     {/*lower division*/}
-    //     <div>
-    //         sds
-    //     </div>
-    //  </div>
-     }
-  <Container>
-  </Container>
-</Navbar>
-</div>
+     <Navbar className='upperDiv hiddenUpperDiv' expand="lg">    
+     </Navbar>
+   </div>
  );   
 }
 
