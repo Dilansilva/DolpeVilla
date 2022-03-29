cf9935cbec1d559a9c1c115ebe1ec799411828ae
Dilansilva <sdilansilva@gmail.com>
2022-03-29 21:28:47 +0530

    navbar is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,26 +30,34 @@ function Header(){
  return(
   <>
      {width > 480 ? <Navbar className='upperDiv hiddenUpperDiv' expand="lg"></Navbar> : null}
-     <Navbar expand="lg" className="taransParent">
-  <Container className="taransParent">
-    <Navbar.Brand href="#">sample text</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <ToggleButton className="gardientBtn"><b>Primary</b></ToggleButton>
-        <ToggleButton className="gardientBtn">Primary</ToggleButton>
-        <ToggleButton className="gardientBtn">Primary</ToggleButton>
-        <ToggleButton className="gardientBtn">Primary</ToggleButton>
-      </Nav>
-      
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-   </>
+      <Navbar className="taransParent">
+        <Container className="taransParent">
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <ToggleButton className="gardientBtn"><b>COVID-19 UPDATES</b></ToggleButton>
+              <ToggleButton className="gardientBtn">GALLERY</ToggleButton>
+              <ToggleButton className="gardientBtn">CONTACT US</ToggleButton>
+              <ToggleButton className="gardientBtn">PROMOTIONS</ToggleButton>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Navbar className="taransParent">
+        <Container className="taransParent">
+          <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <ToggleButton className="gardientBtn"><b>HOME</b></ToggleButton>
+              <ToggleButton className="gardientBtn">LOCATION</ToggleButton>
+              <ToggleButton className="gardientBtn">ACCOMMODATION</ToggleButton>
+              <ToggleButton className="gardientBtn">FACILITIES</ToggleButton>
+              <ToggleButton className="gardientBtn">DINNING</ToggleButton>
+              <ToggleButton className="gardientBtn">THINGS TO DO</ToggleButton>              
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  </>
+ 
  );   
 }
 
