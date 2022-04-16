c88ae93493111fa2c8ec65f53d5f1af75850cf48
Dilansilva <sdilansilva@gmail.com>
2022-04-16 17:43:25 +0530

    Basic model is done

--- src/components/MobilePopUp.js
+++ src/components/MobilePopUp.js
@@ -1,18 +1,13 @@
-import {useState,useEffect} from 'react';
+import {useState} from 'react';
 import {Button,Modal} from 'react-bootstrap';
 
 const MobilePopUp = () => {
     const [show, setShow] = useState(true);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
   
     return (
       <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch static backdrop modal
-        </Button>
-  
         <Modal
           show={show}
           onHide={handleClose}
@@ -26,8 +21,7 @@ const MobilePopUp = () => {
             I will not close if you click outside me. Don't even try to press
             escape key.
           </Modal.Body>
-          <br/>
-          <br/>
+         
           <Modal.Footer>
            <br/>
           </Modal.Footer>
