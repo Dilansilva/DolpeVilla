6544a1a8114cda21fe973cf33e01b33be8033ca2
Dilansilva <sdilansilva@gmail.com>
2022-04-16 16:48:50 +0530

    nav bar for mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,26 +69,18 @@ function Header(){
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#features">COVID-19 UPDATES</Nav.Link>
+          <Nav.Link href="#pricing">GALLERY</Nav.Link>
+          <Nav.Link href="#features">CONTACT US</Nav.Link>
+          <Nav.Link href="#pricing">PROMOTIONS</Nav.Link>
+          <Nav.Link href="#features">HOME</Nav.Link>
+          <Nav.Link href="#pricing">LOCATION</Nav.Link>
+          <Nav.Link href="#features">ACCOMMODATION</Nav.Link>
+          <Nav.Link href="#pricing">FACILITIES</Nav.Link>
+          <Nav.Link href="#features">DINNING</Nav.Link>
+          <Nav.Link href="#pricing">THINGS TO DO</Nav.Link>
         </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
-        </Nav>
+        
       </Navbar.Collapse>
     </Navbar>
         </>
