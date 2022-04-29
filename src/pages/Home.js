178200d49e988de2ea8a8d89404ca874593ce770
Dilansilva <sdilansilva@gmail.com>
2022-04-29 21:58:15 +0530

    Image is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,52 @@
 import React from "react";
-import backGround from "../assets/MobilePopUpBackGround.jpg";
+import backGround from "../assets/DSC01499.jpg";
 import '../../src/App.css';
+import MiddleBar from "../components/MiddleBar";
 
 function Home() {
     return (
+    <>
         <div className="homeImage" style={{
              backgroundImage: `url(${backGround})`
         }}>
-           <p>nlnln</p>
-           <p>nlnln</p>
-           <p>nlnln</p>
+           {
+               //Middle bar
+           }
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <br/><br/><br/>
+           <MiddleBar/>
         </div>
+        {/*Dolpe Villa Ella, Sri lanka */}
+        <div style={{backgroundColor:'#1F211F'}}>
+        <div className="secondDiv">
+           <br/><br/><br/>
+           <h4 className="dolpeText">Dolpe Villa Ella, Sri lanka</h4>
+           <br/><br/>
+           <p>Located 8 km from Demodara Nine Arch Bridge, 
+           Dolape Villa has accommodations with a restaurant, 
+           a garden and a 24-hour front desk for your convenience. 
+           Complimentary WiFi is offered throughout the property.</p>
+
+           <p>Some of the units have satellite flat-screen TV, 
+           a fully equipped kitchenette with a minibar, 
+           and a private bathroom with a hot tub and bathrobes.</p>
+
+           <p>The lodge offers a continental or à la carte breakfast.</p>
+
+           <p>Dolape Villa has a playground</p>
+
+           <p>A water park is available on site and cycling can 
+           be enjoyed within close proximity of the accommodation.</p>
+           <br/><br/><br/>
+        </div>
+        </div>
+    </>
     );
 }
 
