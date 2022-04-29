0f7717c0d9cb9aa7ed0f3e8e933a8fe8d9152a61
Dilansilva <sdilansilva@gmail.com>
2022-04-29 14:58:06 +0530

    Middle bar is done

--- src/components/MiddleBar.js
+++ src/components/MiddleBar.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Container,Navbar,ToggleButton} from 'react-bootstrap';
 
 function MiddleBar() {
     return(
         <div>
-          <p>lnln</p>
+           <Navbar className="taransParentMiddleBar">
+        <Container >
+         
+          <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-center">
+              <ToggleButton className="gardientBtn"><b>MAKE A RESERVATION</b></ToggleButton>
+              <ToggleButton className="gardientBtn">CHECK IN</ToggleButton>
+              <ToggleButton className="gardientBtn">CHECK OUT</ToggleButton>
+              <ToggleButton className="gardientBtn">SELECT YOUR COUNTRY</ToggleButton>
+              <ToggleButton className="gardientBtn">BOOK NOW</ToggleButton>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
         </div>
     );
 }
