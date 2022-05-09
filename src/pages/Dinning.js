c6f8300681f42faba861836b6b4a6ff9787c20a3
Dilansilva <sdilansilva@gmail.com>
2022-05-09 10:12:38 +0530

    dinning samples are done

--- src/pages/Dinning.js
+++ src/pages/Dinning.js
@@ -15,42 +15,42 @@ import ThirdImg from "../assets/287522687.jpg"
 const Dinnning = () => {
     const [text,setText] = useState('location');
     const middleSHow = () =>{
-        if(text == 'location'){
+        if(text == 'dinning'){
             return(
                 <Container>
-                <Row>
-                    <Col sm={4}> 
+                <Row className="blueAreaRow">
+                    <Col sm={4} className="facilList"> 
                         <div className="d-grid gap-2">
-                            <a className="button" onClick={() => {setText('location')}}>DINNING</a>
-                            <a className="button" onClick={() => {setText('transport')}}>DOLPE RILL RESTAURANT</a>
-                            <a className="button" onClick={() => {setText('weather')}}>GARDEN DINNING</a>
+                            <a className="button" onClick={() => {setText('dinning')}}>DINNING</a>
+                            <a className="button" onClick={() => {setText('restaurant')}}>DOLPE RILL RESTAURANT</a>
+                            <a className="button" onClick={() => {setText('garden')}}>GARDEN DINNING</a>
                         </div>
                     </Col>
                     <Col sm={8}>
-                    <h4 className="dolpeText locationText">Dolape Villa standing on exquisite beauty in Ella, Sri Lanka.</h4>
-                     <p className="smallLocationText">Ella is a small village located among the peaks of Sri Lanka’s 
-                         central highlands. This sleepy village is nestled in a valley peering
-                         straight through the Ella Gap to the plain nearly 1000m below and across 
-                         to the coastline. Apart from the mesmerizing natural views, Ella is 
-                         surrounded by hilly countryside perfect for walks; through tea 
-                         plantations, waterfalls, ancient temples and artefacts. With a handful 
-                         of shops, hotels and guesthouses, Ella still remains a remote hamlet. 
-                         This hidden sanctuary with the perfect climate is probably one of the 
-                         best-kept secrets of the country and is easily accessible from the 
-                         comfort of our Ella resort in Sri Lanka.</p>
+                    <h4 className="dolpeText locationText">Dinning</h4>
+                    <div className="secondDiv">
+                        <p className="smallLocationText">lorem ipsum datad</p>
+                            <div>
+                                <ul className="facilOrdList">
+                                    <li className="facilBeforeList"><span href="#home"><b>sds:</b><span>nkn</span></span></li>
+                                    <li className="facilBeforeList"><span href="#home"> Yoga classes </span></li>
+                                    <li className="facilBeforeList"><span href="#home"> Yoga classes </span></li>
+                                </ul>
+                            </div>
+                    </div>
                     </Col>
                 </Row>
             </Container>
             );
-        } else if(text == 'transport'){
+        } else if(text == 'restaurant'){
             return(
                 <Container>
                 <Row>
                 <Col sm={4}> 
                 <div className="d-grid gap-2">
-                            <a className="button" onClick={() => {setText('location')}}>DINNING</a>
-                            <a className="button" onClick={() => {setText('transport')}}>DOLPE RILL RESTAURANT</a>
-                            <a className="button" onClick={() => {setText('weather')}}>GARDEN DINNING</a>
+                <a className="button" onClick={() => {setText('dinning')}}>DINNING</a>
+                            <a className="button" onClick={() => {setText('restaurant')}}>DOLPE RILL RESTAURANT</a>
+                            <a className="button" onClick={() => {setText('garden')}}>GARDEN DINNING</a>
                         </div>
                     </Col>
                     <Col sm={8}>
@@ -79,15 +79,15 @@ const Dinnning = () => {
                 </Row>
             </Container>
             );
-        } else if(text == 'weather'){
+        } else{
             return(
                 <Container>
                 <Row>
                 <Col sm={4}> 
                 <div className="d-grid gap-2">
-                            <a className="button" onClick={() => {setText('location')}}>DINNING</a>
-                            <a className="button" onClick={() => {setText('transport')}}>DOLPE RILL RESTAURANT</a>
-                            <a className="button" onClick={() => {setText('weather')}}>GARDEN DINNING</a>
+                    <a className="button" onClick={() => {setText('dinning')}}>DINNING</a>
+                            <a className="button" onClick={() => {setText('restaurant')}}>DOLPE RILL RESTAURANT</a>
+                            <a className="button" onClick={() => {setText('garden')}}>GARDEN DINNING</a>
                         </div>
                     </Col>
                     <Col sm={8}>
@@ -99,32 +99,6 @@ const Dinnning = () => {
                              The weather pattern in the locality of the resort is recognised
                               as being the most comfortable in the entire country.
                          </p>
-                    </Col>
-                </Row>
-            </Container>
-            );
-        } else {
-            return(
-                <Container>
-                <Row>
-                    <Col sm={4}> 
-                        <div className="d-grid gap-2">
-                            <a className="button" onClick={() => {setText('location')}}>DINNING</a>
-                            <a className="button" onClick={() => {setText('transport')}}>DOLPE RILL RESTAURANT</a>
-                            <a className="button" onClick={() => {setText('weather')}}>GARDEN DINNING</a>
-                        </div>
-                    </Col>
-                    <Col sm={8}>
-                    <h4 className="dolpeText locationText">Excursion</h4>
-                        <p className="smallLocationText">
-                        Ella has plenty to offer that could keep you fascinated and occupied 
-                        during a few nights’ stay. Apart from the mesmerizing views of mountains 
-                        and tea estates, the region provides shelter to many precious flora and 
-                        fauna. In addition to simply unwinding in the cool climes you could set 
-                        out on a trekking adventure or safari to a nearby wildlife sanctuary as
-                        part of excursions in Ella to discover and experience some of these 
-                        delicate wonders of nature up- close.
-                        </p>
                     </Col>
                 </Row>
             </Container>
@@ -248,15 +222,14 @@ const Dinnning = () => {
         </div>
         {/*Dolpe Villa Ella, Sri lanka */}
         <div style={{backgroundColor:'#1F211F'}}>
-           {middleSHow()}
+           <p>nkn</p>
         </div>
         {/*Blue Area */}
         <div className="blueArea" style={{
              backgroundImage: `url(${Blue})`
         }}>
-           {
-               //Middle bar
-           }
+           <br/><br/><br/><br/>
+               {middleSHow()}
        <Container style={{textAlign:'center'}}>
         <br/>
        <Row className="blueAreaRow">
