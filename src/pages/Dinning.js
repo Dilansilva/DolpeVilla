f0a2191b059f7247385b9f007a765e31c21116c3
dilansilva <sdilansilva@gmail.com>
2022-05-10 20:04:37 +0530

    Garden dinning is done

--- src/pages/Dinning.js
+++ src/pages/Dinning.js
@@ -223,15 +223,11 @@ const Dinnning = () => {
                         ))}
                     </ImageList>
             </div>   
-
-               
-        
             );
-        } else if(text == 'weather'){
+        } else{
             return(
-                <Row className="blueAreaRow">
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                <div style={{display:'flex',alignItems:'center'}}>
+                    <ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={164}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
                             <img
@@ -243,57 +239,7 @@ const Dinnning = () => {
                         </ImageListItem>
                         ))}
                     </ImageList>
-                </Col>
-
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
-                            <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                            />
-                        </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Col>
-        </Row> 
-            );
-        } else {
-            return(
-                <Row className="blueAreaRow">
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
-                            <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                            />
-                        </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Col>
-
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
-                            <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                            />
-                        </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Col>
-        </Row> 
+            </div>   
             );
         }
     }
