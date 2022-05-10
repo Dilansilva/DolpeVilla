5f53a4d0cff0b8a468167eb2c6eb229900643610
dilansilva <sdilansilva@gmail.com>
2022-05-10 20:02:47 +0530

    DOlpe restaurant is done

--- src/pages/Dinning.js
+++ src/pages/Dinning.js
@@ -209,9 +209,8 @@ const Dinnning = () => {
             );
         } else if(text == 'restaurant'){
             return(
-                <Row className="blueAreaRow">
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+            <div style={{display:'flex',alignItems:'center'}}>
+                    <ImageList sx={{ width: '100%', height: 450 }} cols={4} rowHeight={164}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
                             <img
@@ -223,23 +222,10 @@ const Dinnning = () => {
                         </ImageListItem>
                         ))}
                     </ImageList>
-                </Col>
+            </div>   
 
-                <Col sm={6}>
-                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
-                            <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                            />
-                        </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Col>
-        </Row> 
+               
+        
             );
         } else if(text == 'weather'){
             return(
@@ -351,7 +337,9 @@ const Dinnning = () => {
            }
        <Container style={{textAlign:'center'}}>
         <br/>
-            {showPictures()}
+            <div>
+                {showPictures()}
+            </div>
         <br/>
        </Container>
            
