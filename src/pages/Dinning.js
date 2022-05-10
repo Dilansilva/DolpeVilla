f0ad4cf505cb76bc0399dd20457a8a66bd484b8f
dilansilva <sdilansilva@gmail.com>
2022-05-10 19:38:21 +0530

    Dinning page is done

--- src/pages/Dinning.js
+++ src/pages/Dinning.js
@@ -3,7 +3,11 @@ import backGround from "../assets/_1333445.jpg";
 import '../../src/App.css';
 import MiddleBar from "../components/MiddleBar";
 import Blue from "../assets/_1344152.jpg";
+
 import { Col, Container, Row, Card, Button } from "react-bootstrap";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import LastSec from "../assets/MobilePopUpBackGround.jpg";
 
 import FirstImgTrans from "../assets/f682b054e7868718f4167b9243ab3f9c.jpg";
 import SecondImgTrans from "../assets/The_Common_Wanderer_-3.jpg";
@@ -12,8 +16,74 @@ import FirstImg from "../assets/_1344168.jpg";
 import SecondImg from "../assets/_1322232.jpg";
 import ThirdImg from "../assets/287522687.jpg"
 
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+      },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+    },]
+
 const Dinnning = () => {
-    const [text,setText] = useState('location');
+    const [text,setText] = useState('dinning');
     const middleSHow = () =>{
         if(text == 'dinning'){
             return(
@@ -101,97 +171,143 @@ const Dinnning = () => {
     }
 
     const showPictures = () => {
-        if(text == 'location'){
+        if(text == 'dinning'){
             return(
                 <Row className="blueAreaRow">
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={FirstImg} />
-                    </Card>
-                </Col>
+                    <Col sm={6}>
+                    <div><b style={{color:'white'}}>DOLAPE RILL RESTAURANT</b></div><br/>
+                        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                        {itemData.map((item) => (
+                            <ImageListItem key={item.img}>
+                                <img
+                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.title}
+                                loading="lazy"
+                                />
+                            </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </Col>
     
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={SecondImg} />
-                    </Card>
-                </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={ThirdImg} />
-                    </Card>
-                </Col>
-    
+                    <Col sm={6}>
+                    <div><b style={{color:'white'}}>GARDEN DINNING</b></div><br/>
+                        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                        {itemData.map((item) => (
+                            <ImageListItem key={item.img}>
+                                <img
+                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.title}
+                                loading="lazy"
+                                />
+                            </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </Col>
             </Row> 
             );
-        } else if(text == 'transport'){
+        } else if(text == 'restaurant'){
             return(
                 <Row className="blueAreaRow">
-                <Col>
-                    <Card style={{ width: '18rem',background:'none'}}>
-                        <Card.Img variant="top" src={FirstImgTrans}/>
-                    </Card>
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={SecondImgTrans} />
-                    </Card>
+
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={ThirdImgTrans} />
-                    </Card>
-                </Col>
-    
-            </Row> 
+        </Row> 
             );
         } else if(text == 'weather'){
             return(
                 <Row className="blueAreaRow">
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={FirstImg} />
-                    </Card>
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={SecondImg} />
-                    </Card>
+
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={ThirdImg} />
-                    </Card>
-                </Col>
-    
-            </Row> 
+        </Row> 
             );
         } else {
             return(
                 <Row className="blueAreaRow">
-                <Col>
-                    <Card style={{ width: '18rem',background:'none'}}>
-                        <Card.Img variant="top" src={FirstImgTrans}/>
-                    </Card>
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={SecondImgTrans} />
-                    </Card>
+
+                <Col sm={6}>
+                    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </ImageListItem>
+                        ))}
+                    </ImageList>
                 </Col>
-    
-                <Col>
-                    <Card style={{ width: '18rem',background:'none' }}>
-                        <Card.Img variant="top" src={ThirdImgTrans} />
-                    </Card>
-                </Col>
-    
-            </Row> 
+        </Row> 
             );
         }
     }
@@ -227,49 +343,20 @@ const Dinnning = () => {
            {middleSHow()}
         <br/>
        </Container>
+       <div className="blueArea" style={{
+             
+        }}>
+           {
+               //Middle bar
+           }
        <Container style={{textAlign:'center'}}>
         <br/>
-       <Row className="blueAreaRow">
-            <Col>
-                <Card style={{ width: '18rem',background:'none' }}>
-                    <Card.Img variant="top" src={FirstImg} />
-                        <Card.Body>
-                            <Button variant="primary">ACCOMODATION</Button>
-                                <Card.Text className="textSizeAndCOlor">
-                                The resort is made up of several beautifully crafted chalets tucked within the leveled landscape of the plantation.
-                                </Card.Text>
-    
-                        </Card.Body>
-                </Card>
-            </Col>
-
-            <Col>
-                <Card style={{ width: '18rem',background:'none' }}>
-                    <Card.Img variant="top" src={SecondImg} />
-                        <Card.Body>
-                            <Button variant="primary">THINGS TO DO</Button>
-                                <Card.Text className="textSizeAndCOlor">
-                                Ella is probably one of Sri Lanka’s best kept secrets that offers plenty to keep you fascinated during a few nights’ stay. Apart from simply unwinding in cool climes with hypnotic views;                                 </Card.Text>
-    
-                        </Card.Body>
-                </Card>
-            </Col>
-
-            <Col>
-                <Card style={{ width: '18rem',background:'none' }}>
-                    <Card.Img variant="top" src={ThirdImg} />
-                        <Card.Body>
-                            <Button variant="primary">DINNING</Button>
-                                <Card.Text className="textSizeAndCOlor">
-                                Dining at Dolape Villa Ella is a stimulating experience. A truly unique open area restaurant Ella, the ‘Restaurant Dolape Villa’, built on a hillock that overlooks breathtaking scenery, engulfs the guests in a tantalizing dining experience.
-                                </Card.Text>
-    
-                        </Card.Body>
-                </Card>
-            </Col>
-
-        </Row>
+            {showPictures()}
+        <br/>
        </Container>
+           
+        </div>
+           
            
         </div>
     </>
