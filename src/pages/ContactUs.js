ebfb4c8d82e8c242b28a8bd558aa27e9aa5f1581
dilansilva <sdilansilva@gmail.com>
2022-05-13 21:46:17 +0530

    contact us page is done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,10 +3,49 @@ import backGround from "../assets/ella-rock.jpg";
 import '../../src/App.css';
 import MiddleBar from "../components/MiddleBar";
 import Blue from "../assets/_1322204.jpg";
-import { Col, Container, Row, Card, Button, Breadcrumb } from "react-bootstrap";
+import { Col, Container, Row, Card, Button, Breadcrumb, Accordion } from "react-bootstrap";
 import FirstImg from "../assets/_1344168.jpg";
 import SecondImg from "../assets/_1322232.jpg";
 import ThirdImg from "../assets/287522687.jpg"
+
+const faqs = [
+    {
+        header: "What are the Check-in and Check-out times at Dolape Villa ?",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    },
+    {
+        header: "How far is Dolape Villa from the center of Ella ?",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    },
+    {
+        header: "Is Dolape Villa Ella popular with families ?",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    },
+    {
+        header: "Does Dolape Villa have a restaurant on site ?",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    },
+    {
+        header: "What kind of breakfast is served at Dolape Villa ?",
+        body: "Lorem ipsum dolor sit amet"
+    },
+    {
+        header: "What is there to do at Dolape Villa Ella ?",
+        body: "Lorem ipsum dolor sit amet"
+    },
+    {
+        header: "Does Dolape Villa have a hot tub for its guests ?",
+        body: "Lorem ipsum dolor sit amet"
+    },
+    {
+        header: "How much does it cost to say at Dolape Villa Ella ?",
+        body: "Lorem ipsum dolor sit amet"
+    },
+    {
+        header: "What type of room can I book at Dolape Villa Ella ?",
+        body: "Lorem ipsum dolor sit amet"
+    }
+]
 
 const ContactUs = () => {
     return(
@@ -31,6 +70,7 @@ const ContactUs = () => {
                 <div style={{backgroundColor:'#1F211F'}}>
                 <div className="secondDiv"><br/>
                     <h4 className="dolpeText">Contact Us</h4>
+                    <hr style={{color:'#7CA844'}}/>
                     <Container style={{textAlign:'center'}}>
         <br/>
        <Row className="blueAreaRow">
@@ -82,7 +122,24 @@ const ContactUs = () => {
             </Col>
 
         </Row>
-       </Container>
+
+        
+       </Container><br/>
+            <h4 className="dolpeText">FAQ About Dolape Villa</h4>
+                <hr style={{color:'#7CA844'}}/>
+                {
+                    faqs.map((faq) => (
+                        <Accordion defaultActiveKey="0" flush>
+                        <Accordion.Item eventKey="0">
+                             <Accordion.Header>{faq.header}</Accordion.Header>
+                             <Accordion.Body>
+                             {faq.body}
+                             </Accordion.Body>
+                         </Accordion.Item>
+                        </Accordion>
+                    ))
+                }
+                  <br/>
                 </div>
                 </div>
                 {/*Blue Area */}
