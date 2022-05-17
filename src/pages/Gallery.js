dc793c0fda785602994092d460cec936e6be2c81
dilansilva <sdilansilva@gmail.com>
2022-05-17 22:58:46 +0530

    Dolep rill restaurant images are done

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -11,6 +11,136 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { First } from "react-bootstrap/esm/PageItem";
 
+const itemDataRill = [
+  {
+      img: "https://i.postimg.cc/cCsFMfx9/266309325_649201563106196_407574773516081162_n.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/zXbwgd5t/_1322236.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/2yzZXwBW/_1322239.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/NFw4Gm2G/_1333392.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/cJ9M3PKz/_1333393.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/8PcGB6nn/_1333394.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/26xw63Q4/_1333436.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/fbNTKbdt/_1333437.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/c1mYtVVB/_1333442.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/SKkYKdDv/_1333448.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/RhFcZdVB/_1333449.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/sX5LJVpX/_1344181.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/W39kCZmf/_1344185.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/D0FMjtgc/_1344186.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/Qxvpjc22/_1344189.jpg",
+      title: 'never mind'
+  },
+  {
+      img: "https://i.postimg.cc/d1hQNHZC/sdfdfsdf.jpg",
+      title: 'never mind'
+  },
+];
+
+const itemDataGarden = [
+  {
+      img: "https://i.postimg.cc/C5hBwpV1/111537168-314272919932397-3842468518808660940-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/KjDP55mK/111727377-314272979932391-3257877947673588409-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/xdmbRPR0/116744256-314272849932404-3451675875120941001-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/C5x1WCjn/137526812-436967364329618-5345110808949255071-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/6q4s2yK1/137534117-436965417663146-9173200387753449845-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/3R7C06TM/137576998-436967410996280-7870586033234887330-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/BZjtcpQR/137579704-436967400996281-1607976506926667323-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/v8CTPn4T/137606797-436965424329812-7132746721412113500-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/nLqGzxNY/272312560-677714636921555-2386438763310556595-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/14xz4kkw/272674727-677714616921557-6271748510368594815-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/28vQTrSp/272689457-677714600254892-3668276923148260266-n.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/9Xy0nMZB/1333450.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/52s6SRLd/1344151.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/FsjS3pTN/1344152.jpg",
+      title: 'title'
+  },
+  {
+      img: "https://i.postimg.cc/dtMdhpm5/1344187.jpg",
+      title: 'title'
+  }
+]
+
 const itemDataBudget = [
   {
     img: "https://i.postimg.cc/ZRzxF1xY/1344124-518796593e48071df6c2.jpg",
@@ -316,7 +446,7 @@ const Gallery = () => {
                     <Col sm={6}>
                     <div><h4 className="dolpeText">Dolape Rill Restaurant</h4></div><br/>
                         <ImageList sx={{ width: 500, height: 450 }} cols={4} rowHeight={164}>
-                        {itemDataStandard.map((item) => (
+                        {itemDataRill.map((item) => (
                             <ImageListItem key={item.img}>
                                 <img
                                 src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
@@ -332,7 +462,7 @@ const Gallery = () => {
                     <Col sm={6}>
                     <div><h4 className="dolpeText">Garden Dinning</h4></div><br/>
                         <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-                        {itemDataBudget.map((item) => (
+                        {itemDataGarden.map((item) => (
                             <ImageListItem key={item.img}>
                                 <img
                                 src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
