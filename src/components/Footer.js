336196771272fc2ba624e30a735f81e4cb138de5
dilansilva <sdilansilva@gmail.com>
2022-05-20 22:08:20 +0530

    Main footer is done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,36 +4,118 @@ import {MDBFooter,MDBIcon} from 'mdb-react-ui-kit';
 
 function Footer() {
     return (
-          <MDBFooter className='text-center text-white addressDiv'>
-          <div className='container p-4 pb-0'>
-            <section className='mb-4'>
-              <a
-                className='btn btn-primary btn-floating m-1'
-                style={{ backgroundColor: '#3b5998' }}
-                href="https://www.facebook.com/Dolape-Villa-114357656590592"
-                role='button'
-              >
-                <MDBIcon fab icon='facebook-f' />
-              </a>
-    
-              <a
-                className='btn btn-primary btn-floating m-1'
-                style={{ backgroundColor: '#ac2bac' }}
-                href='https://www.instagram.com/dolapevilla_ella/?igshid=YmMyMTA2M2Y='
-                role='button'
-              >
-                <MDBIcon fab icon='instagram' />
-              </a>
-            </section>
+      <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <a href='https://www.facebook.com/Dolape-Villa-114357656590592' className='me-4 text-reset'>
+            <i className='fab fa-facebook-f'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-google'></i>
+          </a>
+          <a href='https://www.instagram.com/dolapevilla_ella/?igshid=YmMyMTA2M2Y=' className='me-4 text-reset'>
+            <i className='fab fa-instagram'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-whatsapp'></i>
+          </a>
+        </div>
+      </section>
+
+      <section className=''>
+        <div className='container text-center text-md-start mt-5'>
+          <div className='row mt-3'>
+            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <i className='fas fa-gem me-3'></i>Company name
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+              </p>
+            </div>
+
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Angular
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  React
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Vue
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Laravel
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <i className='fas fa-home me-3'></i>Dolape Villa,
+                                      Kandekumbura Road,
+                                      Ballaketuwa,
+                                      Ella,
+                                      90090,
+                                      Sri Lanka.
+              </p>
+              <p>
+                <i className='fas fa-envelope me-3'></i>
+              dolapevilla.lbh@gmail.com
+              </p>
+              <p>
+                <i className='fab fa-whatsapp me-3'></i>+94 (0) 777 1735 28
+              </p>
+            </div>
           </div>
-    
-          <div style={{ backgroundColor: '#1F211F',display:"flex",justifyContent:'space-between',  position: 'relative'}}
-               className="footerDiv"
-          >
-            <p className="footerText footerTextMobile"> All Copyrights Reserved by Dolape Villa Ella - © 2022</p>
-              <p className="footerText footerTextMobile" >Website Designed & Developed by D&SDeveloer</p>
-          </div>
-        </MDBFooter>
+        </div>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2022 Copyright:
+        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+        Website Designed & Developed by D&SDeveloer
+        </a>
+      </div>
+    </MDBFooter>
     
       );
 }
