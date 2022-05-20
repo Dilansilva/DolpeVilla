0797c5778b0aacc5cb1ba97b4c26aff6937a8c34
dilansilva <sdilansilva@gmail.com>
2022-05-21 01:10:50 +0530

    middle bar Phone view is done

--- src/components/MiddleBarPhoneView.js
+++ src/components/MiddleBarPhoneView.js
@@ -1,24 +1,25 @@
 import React from "react";
 import {Container,Navbar,Button} from 'react-bootstrap';
+import '../../src/App.css';
 import {useNavigate} from "react-router-dom";
 
 function MiddleBarPhoneView() {
     const navigate = useNavigate();
     return(
         <div style={{textAlign:"center",marginLeft:'25%',marginRight:'25%'}} className="d-grid gap-2">
-            <Button variant="primary" size="lg" style={{margin:'1%'}} onClick={() => {navigate("/bookings")}}>
+            <Button size="lg" style={{margin:'1%',backgroundColor:'transparent',background:'rgba(45, 45, 45, 0.5)'}} onClick={() => {navigate("/bookings")}}>
                 MAKE A RESERVATION
             </Button><br/>
-            <Button variant="primary" size="lg" style={{margin:'1%'}} onClick={() => {navigate("/bookings")}}>
+            <Button  style={{margin:'1%',backgroundColor:'transparent',border:'1px',background:'rgba(45, 45, 45, 0.5)'}}  size="lg" onClick={() => {navigate("/bookings")}}>
             CHECK IN
             </Button><br/>
-            <Button variant="primary" size="lg" style={{margin:'1%'}} onClick={() => {navigate("/bookings")}}>
+            <Button  style={{margin:'1%',backgroundColor:'transparent',border:'1px',background:'rgba(45, 45, 45, 0.5)'}} size="lg" onClick={() => {navigate("/bookings")}}>
             CHECK OUT
             </Button><br/>
-            <Button variant="primary" size="lg" style={{margin:'1%'}} onClick={() => {navigate("/bookings")}}>
+            <Button  style={{margin:'1%',backgroundColor:'transparent',border:'1px',background:'rgba(45, 45, 45, 0.5)'}} size="lg" onClick={() => {navigate("/bookings")}}>
             SELECT YOUR COUNTRY
             </Button><br/>
-            <Button variant="primary" size="lg" style={{margin:'1%'}} onClick={() => {navigate("/bookings")}}>
+            <Button  style={{margin:'1%',backgroundColor:'transparent',border:'1px',background:'rgba(45, 45, 45, 0.5)'}} size="lg"  onClick={() => {navigate("/bookings")}}>
             BOOK NOW
             </Button><br/>
         </div>
