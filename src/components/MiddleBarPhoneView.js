67cb8229a8b078ce6b3fe7a310de232e61432eb4
dilansilva <sdilansilva@gmail.com>
2022-05-21 15:07:47 +0530

    Middle bar is done

--- src/components/MiddleBarPhoneView.js
+++ src/components/MiddleBarPhoneView.js
@@ -6,7 +6,11 @@ import {useNavigate} from "react-router-dom";
 function MiddleBarPhoneView() {
     const navigate = useNavigate();
     return(
-        <div style={{textAlign:"center",padding:'15%',backgroundImage:`url("https://i.postimg.cc/gc7DNV6B/48800644637-e09e7ecc6b-b.jpg")`}} className="d-grid gap-2">
+        <div style={{textAlign:"center"
+                    ,padding:'15%',
+                    backgroundImage:`url("https://i.postimg.cc/gc7DNV6B/48800644637-e09e7ecc6b-b.jpg")`,
+                    backgroundPosition:'bottom 40%'}} className="d-grid gap-2">
+            <h1 className="dolpeMainText">DOLPE VILLA <br/>ELLA</h1>
             <Button size="lg" style={{margin:'1%',backgroundColor:'transparent',background:'rgba(45, 45, 45, 0.5)'}} onClick={() => {navigate("/bookings")}}>
                 MAKE A RESERVATION
             </Button>
