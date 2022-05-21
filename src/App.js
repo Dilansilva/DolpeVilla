dd44a5eb27f0c1702e370882eaf1959687c0b490
dilansilva <sdilansilva@gmail.com>
2022-05-21 16:29:20 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <Router>
       <div>
-        
+        <Header/>
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='location' element={<Location/>}/>
