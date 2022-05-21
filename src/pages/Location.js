aaac69c74461ac017a6b8f3c307803b3dfc6e0fa
dilansilva <sdilansilva@gmail.com>
2022-05-21 20:19:09 +0530

    Location page width is done

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,11 +1,38 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import '../../src/App.css';
 import MiddleBar from "../components/MiddleBar";
 
 import { Col, Container, Row, Card } from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
+function getWindowDimensions() {
+    const { innerWidth: width } = window;
+    return {
+      width
+    };
+  }
 
 function Location() {
+
+    const navigate = useNavigate();
+
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return windowDimensions;
+  }
+
+  const {height,width} = useWindowDimensions();
+
+
     const [text,setText] = useState('location');
     const middleSHow = () =>{
         if(text == 'location'){
