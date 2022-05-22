f1aa60008137d6b260774e4c43ec1d91a272b79e
dilansilva <sdilansilva@gmail.com>
2022-05-22 20:12:02 +0530

    Gallery name change]

---
+++ src/components/GalleryCarousel.js
@@ -0,0 +1,26 @@
+import { Carousel } from "react-bootstrap";
+import React from "react";
+import '../../src/App.css';
+
+const GalleryCarousel = () => {
+    return(
+        <div>
+            <Carousel>
+            {props.items.map((item) => (
+                <Carousel.Item key={item.img}>
+                    <img
+                    className="d-block w-100"
+                    src={`${item.img}`}
+                    alt={item.title}
+                    />
+                </Carousel.Item>
+            ))}
+            </Carousel>
+            <div className="textAlign">
+                <h3>{props.title}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default GalleryCarousel;
