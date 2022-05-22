6c3a4957fb88d3020b68c1edc04590f82d49f3d7
dilansilva <sdilansilva@gmail.com>
2022-05-22 21:55:56 +0530

    logics for mobile

--- src/pages/ThingsToDo.js
+++ src/pages/ThingsToDo.js
@@ -1,10 +1,18 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import '../../src/App.css';
 
 import { ImageList,ImageListItem } from "@mui/material";
 
 import MiddleBar from "../components/MiddleBar";
 import { Col, Container, Row } from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
+function getWindowDimensions() {
+    const { innerWidth: width } = window;
+    return {
+      width
+    };
+  }
 
 const itemDataAround = [
     {
@@ -387,6 +395,25 @@ const itemDataPool = [
 ]
 
 const ThingsToDo = () => {
+
+    const navigate = useNavigate();
+
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return windowDimensions;
+    }
+
+    const {height,width} = useWindowDimensions();
+
     const [text,setText] = useState('things');
     const middleSHow = () =>{
         if(text == 'things'){
