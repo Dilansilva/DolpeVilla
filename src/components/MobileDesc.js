c80c13fd5a08ce4536b9befbc8d0231c8473731c
dilansilva <sdilansilva@gmail.com>
2022-05-22 22:03:15 +0530

    Base descriotion is done

---
+++ src/components/MobileDesc.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MobileDesc = () => {
+    return (
+        <div style={{textAlign:'start',padding:'10%',background:'#030F51'}}>
+            <h1 className="dolpeText locationText">Relaxation , Beautiful view and in luxurious comfort.</h1>
+                <br/><p style={{color:'white'}}>
+                The Villa is made up of several beautifully suites
+                tucked within the levelled landscape. making it one 
+                of the most beautiful views in Sri Lanka. There is ample 
+                space for parking and the suites with the view making it 
+                ideal for a quiet holiday of pure relaxation; for honeymooners 
+                or even for families. Each suites,rooms opens up to scenic views 
+                of the misty mountains,Ravana ella waterfall and is equipped with 
+                all modern amenities that ensure a luxurious, comfortable stay.<br/><br/>
+                Guests have the option of booking this Whole Villa for a group 
+                to accommodate a maximum number of persons per room.
+                </p>
+        </div>
+    );
+}
+
+export default MobileDesc;
