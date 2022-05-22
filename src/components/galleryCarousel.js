4f9953660c23ee8e46b46ff03cdd045458215451
dilansilva <sdilansilva@gmail.com>
2022-05-22 19:50:08 +0530

    Gallery carousel is done

---
+++ src/components/galleryCarousel.js
@@ -0,0 +1,20 @@
+import { Carousel } from "react-bootstrap";
+import React from "react";
+
+const galleryCarousel = () => {
+    return(
+        <Carousel>
+            {props.items.map((item) => (
+                <Carousel.Item key={item.img}>
+                    <img
+                    className="d-block w-100"
+                    src={props.src}
+                    alt={item.title}
+                    />
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+}
+
+export default galleryCarousel;
