734dc897eae07d98a3910e92811acbdb12af7fc8
dilansilva <sdilansilva@gmail.com>
2022-05-22 22:08:49 +0530

    changes

--- src/components/MobileDesc.js
+++ src/components/MobileDesc.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MobileDesc = (props) => {
     return (
-        <div style={{textAlign:'start',padding:'10%',background:'#030F51'}}>
+        <div style={{textAlign:'start',padding:'10%'}}>
             <h1 className="dolpeText locationText">{props.title}</h1>
                 <br/><p style={{color:'white'}}>
                     {props.body}
