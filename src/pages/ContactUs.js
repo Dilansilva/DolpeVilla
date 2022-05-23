b66518d5907415043ff5900ee52ac11eb7253b2a
dilansilva <sdilansilva@gmail.com>
2022-05-23 16:33:59 +0530

    faqs are done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -15,39 +15,39 @@ function getWindowDimensions() {
 const faqs = [
     {
         header: "What are the Check-in and Check-out times at Dolape Villa ?",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+        body: "Check-in at Dolape Villa is from 2:00 PM, and check-out is until 12:00 PM.",
     },
     {
         header: "How far is Dolape Villa from the center of Ella ?",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+        body: "Dolape Villa is 4.5 km from the center of Ella.",
     },
     {
         header: "Is Dolape Villa Ella popular with families ?",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+        body: "Yes, Dolape Villa is popular with guests booking family stays.",
     },
     {
         header: "Does Dolape Villa have a restaurant on site ?",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+        body: "Dolape Villa has 1 restaurant : Dolape Rill Restaurant",
     },
     {
         header: "What kind of breakfast is served at Dolape Villa ?",
-        body: "Lorem ipsum dolor sit amet"
+        body: "Guests staying at Dolape Villa can enjoy a highly-rated breakfast during their stay (guest review score: 8.8) : Continental,Full English/Irish,Vegetarian,Vegan,Halal,Asian,American,Buffet,À la carte"
     },
     {
         header: "What is there to do at Dolape Villa Ella ?",
-        body: "Lorem ipsum dolor sit amet"
+        body: "Dolape Villa offers the following activities/services (charges may apply):,Hot tub/Jacuzzi,Massage,Cycling,Hiking,Playground,Snorkeling,Diving,Fishing,Canoeing,Windsurfing,Solarium,Water park,Evening entertainment,Beauty services,Happy hour,Horseback riding,Bike tours,Facial treatments,Cooking class,Badminton equipment,Water sports facilities on site,Waxing services,Archery,Makeup services,Walking tours,Manicure,Tour or class about local culture,Pedicure,Temporary art galleries,Haircut,Live music/Performance,Hairstyling,Themed dinners,Light therapy,Spa lounge/Relaxation area,Spa/Wellness packages,Back massage,Neck massage,Foot massage,Head massage,Hand massage,Full-body massage,Entertainment staff,Yoga classes"
     },
     {
         header: "Does Dolape Villa have a hot tub for its guests ?",
-        body: "Lorem ipsum dolor sit amet"
+        body: "Yes, there's a hot tub. You can find out more about this and the other facilities at Dolape Villa on this page."
     },
     {
         header: "How much does it cost to say at Dolape Villa Ella ?",
-        body: "Lorem ipsum dolor sit amet"
+        body: "The prices at Dolape Villa may vary depending on your stay (e.g. dates, hotel's policy etc.). To see prices, enter your dates."
     },
     {
         header: "What type of room can I book at Dolape Villa Ella ?",
-        body: "Lorem ipsum dolor sit amet"
+        body: "Room options at Dolape Villa include:,Attic Suite Room,Deluxe Room,Standard Room,Budget Villas"
     }
 ]
 
@@ -157,7 +157,7 @@ const ContactUs = () => {
                         <Accordion defaultActiveKey="0" flush>
                         <Accordion.Item eventKey="1">
                              <Accordion.Header>{faq.header}</Accordion.Header>
-                             <Accordion.Body>
+                             <Accordion.Body style={{color:'black'}}>
                              {faq.body}
                              </Accordion.Body>
                          </Accordion.Item>
