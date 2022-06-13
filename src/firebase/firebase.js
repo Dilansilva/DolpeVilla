f5a1721c194c2547bf33295b98c7b485e9cc7e22
dilansilva <sdilansilva@gmail.com>
2022-06-13 22:41:23 +0530

    firbase db config changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,9 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Fire base products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -17,3 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
+export const db = getFirestore(app);
